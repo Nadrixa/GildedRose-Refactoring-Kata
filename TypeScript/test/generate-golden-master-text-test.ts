a3b6090fbd6de7bff17f3beaf76dc964d00de7f5
Adrian Muñoz Gamez <nadrixa@users.noreply.github.com>
2022-08-25 18:28:38 +0200

    style: applied format

--- TypeScript/test/generate-golden-master-text-test.ts
+++ TypeScript/test/generate-golden-master-text-test.ts
@@ -10,14 +10,13 @@ export function generateGoldenMasterTextTestFor (days: number = 2): string {
     new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
     new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
     new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-    // this conjured item does not work properly yet
     new Item("Conjured Mana Cake", 3, 6)];
-  
-  
+
+
   const gildedRose = new GildedRose(items);
 
   let text = '';
-  
+
   for (let i = 0; i < days; i++) {
     text += `-------- day ${i} --------\n`;
     text += 'name, sellIn, quality\n';
